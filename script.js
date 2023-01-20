d14314ff40770fdaecf41727809497e98e4c1672
alex firmino <alexfirmino67@gmail.com>
2023-01-19 21:34:13 -0300

    created funtion saved files in local storage

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const form = document.querySelector('#form-habits')
 const nwlSetup = new NLWSetup(form)
 
-const data = {
-  run: ['01-01', '01-02', '01-04']
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nwlSetup.data))
 }
 
+function add() {
+
+  const today = new Date().toLocaleString('pt-br').slice('0, -5')
+  const dayExists = nwlSetup.dayExists(today)
+
+  if(dayExists) {
+    alert("Dia já incluso!")
+    return
+  }
+
+  alert('Dia adicionado com sucesso!')
+  nwlSetup.addDay(today)
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 nwlSetup.setData(data)
 nwlSetup.load()
